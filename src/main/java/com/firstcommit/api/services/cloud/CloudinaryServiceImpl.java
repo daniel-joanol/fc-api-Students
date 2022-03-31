75a9417553774f1e90a7428f72386b385f19f120
elodin19 <joanoldaniel@gmail.com>
2022-03-31 20:46:30 +0200

    add readme

--- src/main/java/com/firstcommit/api/services/cloud/CloudinaryServiceImpl.java
+++ src/main/java/com/firstcommit/api/services/cloud/CloudinaryServiceImpl.java
@@ -20,9 +20,9 @@ import java.util.Objects;
 @Service
 public class CloudinaryServiceImpl implements CloudinaryService{
 
-    private final String cloudName = System.getenv("CLOUDINARY_CLOUD_NAME2");
-    private final String apiKey = System.getenv("CLOUDINARY_API_KEY2");
-    private final String apiSecret = System.getenv("CLOUDINARY_API_SECRET2");
+    private final String cloudName = System.getenv("CLOUDINARY_CLOUD_NAME");
+    private final String apiKey = System.getenv("CLOUDINARY_API_KEY");
+    private final String apiSecret = System.getenv("CLOUDINARY_API_SECRET");
     private final Map params = ObjectUtils.asMap(
             "cloud_name", cloudName,
             "api_key", apiKey,
